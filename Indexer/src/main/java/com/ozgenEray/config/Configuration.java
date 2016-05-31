a60b6afc4dce855f9192532f3c38a3b723e16acc
Ozgen <ozgn45@gmail.com>
2016-06-01 01:49:46 +0300

    correct path
    correction

--- Indexer/src/main/java/com/ozgenEray/config/Configuration.java
+++ Indexer/src/main/java/com/ozgenEray/config/Configuration.java
@@ -5,7 +5,7 @@ package com.ozgenEray.config;
  * Change the directories according to your workspace
  */
 public class Configuration {
-	public static final String INDEX_DIRECTORY = "/Users/Ozgen/Desktop/LuceneExampleLuceneExample/index/";
+	public static final String INDEX_DIRECTORY = "/Users/Ozgen/Desktop/LuceneExample/index/";
 	public static final String SOURCE_DIRECTORY_TO_INDEX = "/Users/Ozgen/Desktop/LuceneExample/outcorenlp/";
 	public static final String FIELD_CONTENT = "contents"; // name of the field
 															// to index
